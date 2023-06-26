0d75d227d87caf0576e9d1bda3448b5463370251
Bernhard1987 <demoncleaner1@gmail.com>
2023-06-26 18:09:19 +0200

    Pokemon view on click now working

--- script.js
+++ script.js
@@ -23,12 +23,35 @@ async function loadPokemon(i) {
     let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     let response = await fetch(url);
     currentPokemon = await response.json();
-    renderPokemonInfo();
+    renderPokemonInfo(i);
 }
 
-function renderPokemonInfo() {
+function renderPokemonInfo(i) {
+    document.getElementById('showPokemonBackground').classList.remove('dnone');
     let name = currentPokemon['name'];
-    document.getElementById('name').innerHTML = `${name}`;
+    document.getElementById('showPokemon').innerHTML = '';
+    document.getElementById('showPokemon').innerHTML += `
+            <div class="showPokemonHeadline">
+                <h3 id="name">${name}</h3>
+                <h3>#${i}</h3>
+            </div>
+            <div class="normalView">
+                <h3>Normal View:</h3>
+                <img src="" alt="Normal Front View" id="currentFront">
+                <img src="" alt="Normal Back View" id="currentBack">
+            </div>
+            <div class="shinyView">
+                <h3>Shiny View:</h3>
+                <img src="" alt="Shiny Front View" id="currentFrontShiny">
+                <img src="" alt="Shiny Back View" id="currentBackShiny">
+            </div>
+            <div class="stats">
+                <ul id="statsList">
+                </ul>
+            </div>
+    `;
+
+    // document.getElementById('name').innerHTML = `${name}`;
 
     generateSprites();
     generateStats();
@@ -54,4 +77,8 @@ function generateStats() {
     }
 
     console.log(stats);
+}
+
+function hidePokemon() {
+    document.getElementById('showPokemonBackground').classList.add('dnone');
 }
